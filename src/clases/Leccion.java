87fb51ced3d22e868bb4dd403541c7dddca6fb1b
Sebastián Torres <st141273@gmail.com>
2025-11-07 15:28:46 -0500

    Clases necesarias para el proyecto

---
+++ src/clases/Leccion.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Leccion {
+    private String titulo, descripcion;
+    
+    public Leccion(String titulo, String descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
