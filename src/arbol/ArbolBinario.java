cfc9c6e4f1bdb7e940b5bff0770e777d2dc10b18
Sebastián Torres <sebastiantorres1129733@correo.itm.edu.co>
2025-11-08 18:17:29 -0500

    Se agregó el código para la clase ArbolBinario, estará así por el momento debido a cuestiones técnicas.

--- src/arbol/ArbolBinario.java
+++ src/arbol/ArbolBinario.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package arbol;
 
-/**
- *
- * @author Sebastian
- */
+/*
+   Este será la clase en donde se almacenará y creará el curso virtual, manteniendo
+   la estructura de un árbol binario. También estarán los métodos que permitirán
+   las operaciones básicas de un árbol binario, como: insertar elementos, eliminar
+   un elemento, buscar un elemento, etc.
+*/
+
+import clases.Curso;
+
 public class ArbolBinario {
+    private Nodo<Curso> raiz;
+    private int tamanioArbol = 0;
+    private int contadorID = 1;
     
+    public ArbolBinario() {
+    }
+    
+    public void insertarCurso(String titulo, String descripcion) {
+        if (this.raiz == null) {
+            Curso nuevoCurso = new Curso(titulo, descripcion);
+            
+            this.raiz = new Nodo<>(nuevoCurso);
+            this.raiz.getDato().setID(contadorID);
+            this.contadorID++;
+        } else {
+            System.out.println("Ya hay un curso creado. No se puede añadir un curso dentro de otro curso.");
+        }
+    }
 }
