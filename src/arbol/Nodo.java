428078bead1c8533119c52fb8183e75d88c4d88e
Sebastián Torres <st141273@gmail.com>
2025-11-08 12:32:43 -0500

    Se creó la clase Nodo en la cual estará basado el árbol binario, es un nodo genérico para que reciba cualquier tipo de clase y no haya problemas.

--- src/arbol/Nodo.java
+++ src/arbol/Nodo.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package arbol;
 
-/**
- *
- * @author Sebastian
- */
-public class Nodo {
+public class Nodo<T> {
+    private T dato;
+    private Nodo<T> nodoIzquierdo;
+    private Nodo<T> nodoDerecho;
     
+    public Nodo(T dato) {
+        this.dato = dato;
+        this.nodoIzquierdo = null;
+        this.nodoDerecho = null;
+    }
+
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public Nodo<T> getNodoIzquierdo() {
+        return nodoIzquierdo;
+    }
+
+    public void setNodoIzquierdo(Nodo<T> nodoIzquierdo) {
+        this.nodoIzquierdo = nodoIzquierdo;
+    }
+
+    public Nodo<T> getNodoDerecho() {
+        return nodoDerecho;
+    }
+
+    public void setNodoDerecho(Nodo<T> nodoDerecho) {
+        this.nodoDerecho = nodoDerecho;
+    }
 }
